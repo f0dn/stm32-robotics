133a3766213c783b8f3f7538de50315b32208e73
Flint Mueller <flintthecuber@gmail.com>
2026-04-03 18:28:09 -0400

    analogWrite to send motor powers

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
+#include "wiring_analog.h"
 #include <Arduino.h>
 
 #define NUM_MOTORS 8
 
-const uint16_t MOTOR_PINS[NUM_MOTORS] = {GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10,
+const uint16_t MOTOR_PINS[NUM_MOTORS] = {GPIO_PIN_8,  GPIO_PIN_9,  GPIO_PIN_10,
                                          GPIO_PIN_11, GPIO_PIN_12, GPIO_PIN_13,
                                          GPIO_PIN_14, GPIO_PIN_15};
 
@@ -15,23 +16,20 @@ const uint16_t MOTOR_PINS[NUM_MOTORS] = {GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10,
   ```
   where m_i is the value of the i-th motor, in the range [0, 255].
 */
-class Rx
-{
-public:
+class Rx {
+  public:
     bool isValid = true;
     int32_t motorValues[NUM_MOTORS];
 
-    Rx(const String &csv)
-    {
+    Rx(const String &csv) {
         // Serial.println("DEBUG, parsing CSV string: " + csv);
         // parse exactly MOTOR_NUMBERS values from the CSV string
         int startIndex = 0;
-        for (int motorNum = 0; motorNum < NUM_MOTORS; motorNum++)
-        {
+        for (int motorNum = 0; motorNum < NUM_MOTORS; motorNum++) {
             int commaIndex = csv.indexOf(',', startIndex);
-            if (commaIndex == -1 && motorNum < NUM_MOTORS - 1)
-            {
-                Serial.println("DEBUG, not enough values in CSV string: " + csv + " at motor number: " + String(motorNum));
+            if (commaIndex == -1 && motorNum < NUM_MOTORS - 1) {
+                Serial.println("DEBUG, not enough values in CSV string: " +
+                               csv + " at motor number: " + String(motorNum));
                 // not enough values in the CSV string
                 isValid = false;
                 return;
@@ -44,33 +42,39 @@ public:
     }
 };
 
-void setup()
-{
+void setup() {
     Serial.begin(38400);
     pinMode(LED_BUILTIN, OUTPUT);
+
+    for (int motorNum = 0; motorNum < NUM_MOTORS; motorNum++)
+        analogWrite(MOTOR_PINS[motorNum], 1500);
 }
 
-void loop()
-{
-    if (Serial.available() > 0)
-    {
+void loop() {
+    if (Serial.available() > 0) {
+
         String csv = Serial.readStringUntil('\n');
         if (csv.length() == 0)
             return;
 
         Rx rx(csv);
 
-        if (!rx.isValid)
-        {
+        if (!rx.isValid) {
             Serial.println("WARN, invalid CSV string received: " + csv);
             return;
         }
 
         for (int motorNum = 0; motorNum < NUM_MOTORS; motorNum++)
-            digitalWrite(MOTOR_PINS[motorNum], rx.motorValues[motorNum] > 0 ? HIGH : LOW);
+            analogWrite(
+                MOTOR_PINS[motorNum],
+                rx.motorValues[motorNum]); // this hasnt been tested might need
+                                           // to send 20000ms blocks instead.
+                                           // see old code
 
-        Serial.printf("DEBUG, received motor values: %ld, %ld, %ld, %ld, %ld, %ld, %ld, %ld\n",
-                      rx.motorValues[0], rx.motorValues[1], rx.motorValues[2], rx.motorValues[3],
-                      rx.motorValues[4], rx.motorValues[5], rx.motorValues[6], rx.motorValues[7]);
+        Serial.printf("DEBUG, received motor values: %ld, %ld, %ld, %ld, %ld, "
+                      "%ld, %ld, %ld\n",
+                      rx.motorValues[0], rx.motorValues[1], rx.motorValues[2],
+                      rx.motorValues[3], rx.motorValues[4], rx.motorValues[5],
+                      rx.motorValues[6], rx.motorValues[7]);
     }
 }
